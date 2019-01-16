a4fb0446bf93ffcede0a97a248cc162370663d76
konrad.rojecki <konrad@rojecki.it>
2019-01-16 22:47:18 +0100

    Add screen resolver

---
+++ Game/Core/Screen/ScreenResolver.py
@@ -0,0 +1,13 @@
+from Utils.Helper.ClassProvider import ClassProvider
+
+
+class ScreenResolver:
+
+    MENU_SCREEN = 'Game.Core.Screen.MenuScreen.MenuScreen'
+    GAME_SCREEN = 'Game.Core.Screen.GameScreen.GameScreen'
+
+    ON_START_SCREEN = MENU_SCREEN
+
+    @staticmethod
+    def create_screen(screen_name, configuration):
+        return ClassProvider.provide_class(screen_name)(configuration)
