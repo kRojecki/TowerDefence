b2337f20dab307ab4abaa45e58ac9bd598e3e59f
konrad.rojecki <konrad@rojecki.it>
2019-01-27 12:56:44 +0100

    Remove label click action Add font name to configuration file

--- Game/Core/UI/Elements/Abstract/Label.py
+++ Game/Core/UI/Elements/Abstract/Label.py
@@ -1,5 +1,6 @@
 import pygame
 
+from Configuration.Configuration import Configuration
 from Core.UI.Elements.Abstract.UIElement import UIElement
 from Objects.Abstracts.Pointerable import Pointerable
 from Utils.Constant import Color
@@ -10,23 +11,21 @@ class Label(UIElement, Pointerable):
     _font_color = Color.WHITE
     _prefix = ""
 
-    _font_name = "ubuntumono"
+    _font_name = None
 
-    def __init__(self, prefix, position, font_size=16):
+    def __init__(self, position, prefix, font_size=12):
+        self._font_name = Configuration.get_str('DISPLAY', 'font.name')
         self._font = pygame.font.SysFont(self._font_name, font_size)
         self._position = position
         self._prefix = prefix
 
     def draw(self, screen):
         super().draw(screen)
-        label = self._font.render(self._get_content_to_show(), 1, self._font_color)
+        label = self._font.render(self._get_content_to_show(), False, self._font_color)
         screen.blit(label, self._position)
 
     def _get_content_to_show(self):
         return self._prefix
-
-    def _click_action(self, pressed):
-        print('Button pressed!')
 
     def get_label_size(self):
         return self._font.size(self._get_content_to_show())
